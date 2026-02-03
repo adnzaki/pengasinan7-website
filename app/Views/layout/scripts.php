bfa8946a8807b263264707b5be74a2751635ce78
Adnan Zaki <adnzaki@outlook.com>
2026-02-03 15:35:58 +0700

    refactor main layout

---
+++ app/Views/layout/scripts.php
@@ -0,0 +1,17 @@
+<script src="<?= base_url('assets/js/jquery.js'); ?>"></script>
+<script src="<?= base_url('assets/js/bootstrap.bundle.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/purecounter.js'); ?>"></script>
+<script src="<?= base_url('assets/js/range-slider.js'); ?>"></script>
+<script src="<?= base_url('assets/js/nice-select.js'); ?>"></script>
+<script src="<?= base_url('assets/js/swiper-bundle.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/isotope-pkgd.js'); ?>"></script>
+<script src="<?= base_url('assets/js/slick.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/wow.js'); ?>"></script>
+<script src="<?= base_url('assets/js/countdown.js'); ?>"></script>
+<script src="<?= base_url('assets/js/magnific-popup.js'); ?>"></script>
+<script src="<?= base_url('assets/js/imagesloaded-pkgd.js'); ?>"></script>
+<script src="<?= base_url('assets/js/parallax.js'); ?>"></script>
+
+<!-- Custom JS -->
+<script src="<?= base_url('assets/js/slider.js'); ?>"></script>
+<script src="<?= base_url('assets/js/main.js'); ?>"></script>
