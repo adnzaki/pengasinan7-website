08d1ca0ad8215477b7c30a6c1c47f1f46e9a276d
Adnan Zaki <adnzaki@outlook.com>
2026-07-06 14:37:18 +0700

    add SPMB button to the mobile menu

--- app/Views/layout/mobile-menu.php
+++ app/Views/layout/mobile-menu.php
@@ -13,8 +13,18 @@
                 cras pulvinar suspen.</p>
         </div>
         <div class="it-menu-mobile d-xl-none"></div>
+        <a href="<?= base_url('pendaftaran') ?>" style="width: 75%;margin-top: -60px;position: absolute;" class="it-btn-yellow border-radius-100">
+            <span>
+                <span class="text-1">Pendaftaran</span>
+                <span class="text-2">Pendaftaran</span>
+            </span>
+            <i>
+                <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15.0544 8.1364C15.4058 7.78492 15.4058 7.21508 15.0544 6.8636L9.3268 1.13604C8.97533 0.784567 8.40548 0.784567 8.05401 1.13604C7.70254 1.48751 7.70254 2.05736 8.05401 2.40883L13.1452 7.5L8.05401 12.5912C7.70254 12.9426 7.70254 13.5125 8.05401 13.864C8.40548 14.2154 8.97533 14.2154 9.3268 13.864L15.0544 8.1364ZM0.417969 7.5V8.4H14.418V7.5V6.6H0.417969V7.5Z" fill="currentcolor" />
+                </svg>
+            </i>
+        </a>
         <div class="itoffcanvas__info">
-            <h3 class="offcanva-title">Get In Touch</h3>
             <div class="it-info-wrapper mb-20 d-flex align-items-center">
                 <div class="itoffcanvas__info-icon">
                     <a href="#"><i class="fal fa-envelope"></i></a>
